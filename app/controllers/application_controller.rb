d17d2f76fa10a61eb72818718909d89fc798444d
felixNyalenda <felixasejunior@gmail.com>
2023-06-02 15:58:47 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    game = Game.all.order(:id).limit(10)
+     game.to_json
+  end
+
+  get "/games/:id" do
+    id = params[:id]
+    game = Game.find(id)
+    game.to_json(only: [:id, :title, :genre, :price], include: {reviews: { only: [:id, :comment, :score], include: { user: { only: [:id, :name] } } } })
   end
 
 end
